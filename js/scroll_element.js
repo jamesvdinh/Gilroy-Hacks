579723466d91a5db1b119a90241f13fa9ab2f39e
James Dinh <jamesdinh77@protonmail.com>
2022-06-03 01:42:59 -0700

    Added animations

---
+++ js/scroll_element.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('slide-animation');
+      }
+    });
+  });
+  
+  observer.observe(document.querySelector('#event-main-box'));
+  observer.observe(document.querySelector('#timeline'));
+  observer.observe(document.querySelector('#signup-article'));
+  observer.observe(document.querySelector('#rules-description'));
+  observer.observe(document.querySelector('#guidelines'));
+  observer.observe(document.querySelector('#code-of-conduct'));
+  observer.observe(document.querySelector('#photo-release-container'));
+  observer.observe(document.querySelector('#prizes-container'));
+  observer.observe(document.querySelector('#team-container'));
