6b79d9de583c97736c2c02f2b6c2e963de243043
James Dinh <jamesdinh77@protonmail.com>
2022-06-05 02:23:40 -0700

    Added checkbox localStrage

--- js/data_checkbox.js
+++ js/data_checkbox.js
@@ -1,11 +1,15 @@
-function saveData() {
-    var checkBox = document.getElementById("01");
-
-    if (checkBox.checked == true){
-        localStorage.setItem('checked', 'true')
+var i;
+var checkboxes = document.querySelectorAll('input[type=checkbox]');
+function save() {
+    for (i = 0; i < checkboxes.length; i++) {
+        localStorage.setItem(checkboxes[i].value, checkboxes[i].checked);
     }
-    else {
-        localStorage.setItem('checked', 'false')
+}
+window.onload = function() {
+    load();
+};
+function load() {
+    for (i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true:false;
     }
-    console.log(localStorage);
 }
--- js/filldata_checkbox.js
+++
@@ -1,6 +0,0 @@
-function loadData() {
-    if (localStorage.getItem('checked') == 'true') {
-        document.getElementById("01").checked = true;
-    }
-    console.log("test");
-}
