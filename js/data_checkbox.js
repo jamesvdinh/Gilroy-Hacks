b0ccaa1408ed371ddf9e2ae17aa72cffafddc1bc
James Dinh <jamesdinh77@gmail.com>
2022-06-09 16:55:46 -0700

    Fixed Mobile functionality

--- js/data_checkbox.js
+++ js/data_checkbox.js
@@ -1,5 +1,5 @@
 var i;
-var checkboxes = document.querySelectorAll('input[type=checkbox]');
+var checkboxes = document.querySelectorAll('#signup-container input[type=checkbox]');
 function save() {
     for (i = 0; i < checkboxes.length; i++) {
         localStorage.setItem(checkboxes[i].value, checkboxes[i].checked);
@@ -13,13 +13,7 @@ function load() {
         checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true:false;
     }
 }
-function check01() {
-    document.getElementById('01').checked = true;
-}
-function check02() {
-    document.getElementById('02').checked = true;
-}
-function checkt(id) {
+function check(id) {
     document.getElementById(id).checked = true;
 }
 function notifitcation_popup() {
@@ -28,10 +22,10 @@ function notifitcation_popup() {
     {
         if (localStorage.getItem(checkboxes[i].value) != true) {
             verify = false;
+            break;
         }
     }
     if (verify) {
-        document.getElementById("notification").style.visibility = "visible";
-        
+        document.getElementById('notification').style.visibility = visible;
     }
 }
