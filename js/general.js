17c14013f14bc7016e2c46d40ddce81d5b9d7bc9
James Dinh <jamesdinh77@gmail.com>
2022-07-18 16:46:17 -0700

    Added Tooltips

--- js/general.js
+++ js/general.js
@@ -42,60 +42,6 @@ document.addEventListener('DOMContentLoaded', function(){
     setAttributes(bryce, {"href": "mailto:brycemankovsky" + "@" + "gmail.com?subject=Gilroy Hacks", "target": "_blank"});
     bryce.innerHTML = "brycemankovsky" + "@" + "gmail.com";
 
-    // Checkbox Local Storage
-    var i;
-    var checkboxes = document.querySelectorAll('#signup-container input[type=checkbox]');
-    var verify = true;
-    function save() {
-        for (i = 0; i < checkboxes.length; i++) {
-            localStorage.setItem(checkboxes[i].value, checkboxes[i].checked);
-        }
-    }
-    window.onload = function() {
-        load();
-    };
-    function load() {
-        for (i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true:false;
-        }
-    }
-    function check(id) {
-        document.getElementById(id).checked = true;
-    }
-    function uncheck(id) {
-        document.getElementById(id).checked = false;
-    }
-    function notification(id) {
-        if (localStorage.getItem(checkboxes[id].value) != true) { verify = false; }
-        if (verify) { document.getElementById('notification').style.visibility = visible; }
-    }
-
-    // function btnToggle() {
-    //     if (document.getElementById('#check').checked) {
-    //         document.getElementById('#check').checked = false;
-    //     }
-    //     else {
-    //         document.getElementById('#check').checked = true;
-    //     }
-    // }
-    // document.getElementById('#check').addEventListener('click', function (event) {
-    //     if (!document.getElementById('check').checked) {
-    //         event.stopPropagation();
-    //     }
-    // });
-    // window.onclick = function(event) {
-    //     if (!event.target.matches('check')) {
-    //         if (document.getElementById('check').checked) {
-    //             document.getElementById('check').checked = false;
-    //         }
-    //     }
-    // }
-
-    // document.getElementById("navList").addEventListener('click', function (event) {
-    //     if (document.getElementById('check').checked) {
-    //         event.stopPropagation();
-    //     }});
-
     // Scroll Element
     // Creating an observer when the user views that element
     const observer = new IntersectionObserver(entries => {
@@ -257,3 +203,30 @@ window.addEventListener("load", function() {
 });
 
 // General functions
+// Checkbox Local Storage
+var i;
+var checkboxes = document.querySelectorAll('#signup-container input[type=checkbox]');
+var verify = true;
+function save() {
+    for (i = 0; i < checkboxes.length; i++) {
+        localStorage.setItem(checkboxes[i].value, checkboxes[i].checked);
+    }
+}
+window.onload = function() {
+    load();
+};
+function load() {
+    for (i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true:false;
+    }
+}
+function check(id) {
+    document.getElementById(id).checked = true;
+}
+function uncheck(id) {
+    document.getElementById(id).checked = false;
+}
+function notification(id) {
+    if (localStorage.getItem(checkboxes[id].value) != true) { verify = false; }
+    if (verify) { document.getElementById('notification').style.visibility = visible; }
+}
