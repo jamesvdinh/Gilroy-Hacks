706b280d700e5b6dda44974b13e53baee115e423
James Dinh <jamesdinh77@gmail.com>
2022-07-22 09:59:28 -0700

    Added emails

--- js/general.js
+++ js/general.js
@@ -22,6 +22,10 @@ document.addEventListener('DOMContentLoaded', function(){
     setAttributes(isaac, {"href": "mailto:ihwang125" + "@" + "gmail.com?subject=Gilroy Hacks", "target": "_blank"});
     isaac.innerHTML = "ihwang125" + "@" + "gmail.com";
 
+    var jonathan = document.getElementById("email-replace-jonathan");
+    setAttributes(jonathan, {"href": "mailto:jonathantessmann" + "@" + "gmail.com?subject=Gilroy Hacks", "target": "_blank"});
+    jonathan.innerHTML = "jonathantessmann" + "@" + "gmail.com";
+
     var vijay = document.getElementById("email-replace-vijay");
     setAttributes(vijay, {"href": "mailto:vijaykethanaboyina" + "@" + "gmail.com?subject=Gilroy Hacks", "target": "_blank"});
     vijay.innerHTML = "vijaykethanaboyina" + "@" + "gmail.com";
