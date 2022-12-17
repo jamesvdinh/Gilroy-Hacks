20b93e1aef2f54ff21a6347fc03bfa514e8cebe1
James Dinh <jamesdinh77@gmail.com>
2022-12-16 19:13:03 -0800

    Added modals to schedule

--- js/general.js
+++ js/general.js
@@ -291,6 +291,89 @@ function notification(id) {
     if (verify) { document.getElementById('notification').style.visibility = visible; }
 }
 
+// Disable Scroll (temporarily)
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+var openClick = false;
+var openModal = false;
+var currentModal;
+
+function closeModal(id) {
+	document.getElementById(id).style.display = "none";
+  document.getElementById("modal-overlay").style.display = "none";
+  enableScroll();
+  openModal = false;
+}
+
+function showModal(id) {
+	document.getElementById(id).style.display = "block";
+  document.getElementById("modal-overlay").style.display = "block";
+  document.getElementById("modal-overlay").style.opacity = 1;
+  document.getElementById("modal-overlay").classList.add("modal-animation");
+  disableScroll();
+  openClick = true;
+  openModal = true;
+  currentModal = id;
+}
+
+
+document.addEventListener(
+  "click",
+  function (event) {
+    // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+    if (!openClick && openModal == true) {
+      if (
+        event.target.matches(".button-close-modal") ||
+        !event.target.closest(currentModal)
+      ) {
+        closeModal(currentModal);
+      }
+    }
+    openClick = false;
+  },
+  false
+); 
+
 var options = {
   series: [{
     name: 'Idea',
