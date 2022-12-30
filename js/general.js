f891c2bdf76f1c4292c75730f4323634cbd0cc89
James Dinh <jamesdinh77@gmail.com>
2022-12-30 15:31:59 -0800

    Removed unnecessary objects

--- js/general.js
+++ js/general.js
@@ -157,8 +157,7 @@ document.addEventListener('DOMContentLoaded', function(){
     '#second-podium',
     '#first-podium',
     '#third-podium',
-    '#chart',
-    '#prize-box',
+    // '#chart',
     '#administration',
     '#logistics',
     '#outreach',
@@ -537,82 +536,82 @@ function slide(wrapper, items, prev, next) {
   }
 }
 
-var options = {
-  series: [{
-    name: 'Idea',
-    data: [7, 9, 6, 10, 14],
-  }, {
-    name: 'Implementation',
-    data: [11, 6, 11, 3, 9]
-  }, {
-    name: 'Presentation',
-    data: [12, 7, 12, 11, 8]
-  }, {
-    name: 'Overall',
-    data: [10, 6, 9, 7, 11],
-  }],
-  chart: {
-    type: 'bar',
-    height: 350,
-    stacked: true,
-    background: '#0d1117'
-  },
-  plotOptions: {
-    bar: {
-      horizontal: true,
-    },
-  },
-  stroke: {
-    width: 1,
-    colors: ['#fff']
-  },
-  title: {
-    text: 'Team Scores - Summer 2022',
-    style: {
-      color: '#fff'
-    }
-  },
-  xaxis: {
-    categories: ['Team 2', 'Team 3', 'Team 4', 'Team 6', 'Team 7'],
-    labels: {
-      formatter: function (val) {
-        return val + ""
-      },
-      style: {
-        colors: '#fff'
-      }
-    },
-  },
-  yaxis: {
-    labels: {
-        style: {
-          colors: '#fff'
-        }
-    },
-    title: {
-      text: undefined
-    },
-  },
-  tooltip: {
-    y: {
-      formatter: function (val) {
-        return val + ""
-      }
-    },
-    theme: 'dark'
-  },
-  fill: {
-    opacity: 1
-  },
-  legend: {
-    position: 'top',
-    horizontalAlign: 'left',
-    offsetX: 40,
-    labels: {
-      colors: '#fff',
-  },
-  },
-  };
+// var options = {
+//   series: [{
+//     name: 'Idea',
+//     data: [7, 9, 6, 10, 14],
+//   }, {
+//     name: 'Implementation',
+//     data: [11, 6, 11, 3, 9]
+//   }, {
+//     name: 'Presentation',
+//     data: [12, 7, 12, 11, 8]
+//   }, {
+//     name: 'Overall',
+//     data: [10, 6, 9, 7, 11],
+//   }],
+//   chart: {
+//     type: 'bar',
+//     height: 350,
+//     stacked: true,
+//     background: '#0d1117'
+//   },
+//   plotOptions: {
+//     bar: {
+//       horizontal: true,
+//     },
+//   },
+//   stroke: {
+//     width: 1,
+//     colors: ['#fff']
+//   },
+//   title: {
+//     text: 'Team Scores - Summer 2022',
+//     style: {
+//       color: '#fff'
+//     }
+//   },
+//   xaxis: {
+//     categories: ['Team 2', 'Team 3', 'Team 4', 'Team 6', 'Team 7'],
+//     labels: {
+//       formatter: function (val) {
+//         return val + ""
+//       },
+//       style: {
+//         colors: '#fff'
+//       }
+//     },
+//   },
+//   yaxis: {
+//     labels: {
+//         style: {
+//           colors: '#fff'
+//         }
+//     },
+//     title: {
+//       text: undefined
+//     },
+//   },
+//   tooltip: {
+//     y: {
+//       formatter: function (val) {
+//         return val + ""
+//       }
+//     },
+//     theme: 'dark'
+//   },
+//   fill: {
+//     opacity: 1
+//   },
+//   legend: {
+//     position: 'top',
+//     horizontalAlign: 'left',
+//     offsetX: 40,
+//     labels: {
+//       colors: '#fff',
+//   },
+//   },
+//   };
 
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
+//   var chart = new ApexCharts(document.querySelector("#chart"), options);
+//   chart.render();
